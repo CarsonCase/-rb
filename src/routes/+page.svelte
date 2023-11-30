c837d9ba1c884c7cc75f96214aff81d3ed82173e
Carson Case <carsoncase@gmail.com>
2023-11-30 14:32:52 -0800

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,6 +48,9 @@ function getData(){
 }
 
 function loadChart(){
+    Chart.defaults.color = '#FFF';
+    Chart.defaults.borderColor = '#62A87C';
+
     if(chartObj){
         chartObj.destroy();
     }
@@ -64,8 +67,8 @@ function loadChart(){
             datasets: [
             {
                 label: "Portfolio Value",
-                backgroundColor: "rgb(255, 99, 132)",
-                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "#4ade80",
+                borderColor: "#4ade80",
                 data: data
             }
             ]
@@ -164,30 +167,30 @@ function handleSliderChange(index: number, event: Event) {
 </script>
 
 
-<div class="grid grid-cols-3 grid-rows-4 h-screen">
-    <div class="bg-red-200 row-span-4">
+<div class="grid grid-cols-3 grid-rows-4 h-screen text-white">
+    <div class="bg-blue-500 row-span-4">
         <div class="grid justify-items-center">
             <h1 class="p-10 text-6xl">Ürb Finance</h1>
         </div>
-        <div class="p-10 grid gap-4">
+        <div class="p-10 grid gap-4 text-black">
             <div class="grid">
                 <label for="starting">Starting Cash</label>
-                <input bind:value={startingCash} type="number" name="starting" step=100>
+                <input class="rounded-md border-0" bind:value={startingCash} type="number" name="starting" step=100>
             </div>
 
             <div class="grid">
                 <label for="monthly">Cash added monthly</label>
-                <input bind:value={cashPerMonth} type="number" name="montly" step=100>
+                <input class="rounded-md border-0" bind:value={cashPerMonth} type="number" name="montly" step=100>
             </div>
 
             <div class="grid">
                 <label for="months">Number of Months</label>
-                <input bind:value={numberOfMonths} type="number" name="months">
+                <input class="rounded-md border-0" bind:value={numberOfMonths} type="number" name="months">
             </div>
 
-            <div class="grid">
+            <div class="">
                 <label for="isRandom">Use Random numbers based on asset mean/std</label>
-                <input bind:checked={isRandom} type="checkbox" name="isRandom">
+                <input class="rounded-md border-0" bind:checked={isRandom} type="checkbox" name="isRandom">
             </div>
                 {#if assetsLoaded}
                     {#each assets as asset, index}
@@ -197,6 +200,7 @@ function handleSliderChange(index: number, event: Event) {
                                 <p>{asset.portfolioWeight}%</p>
                             </div>
                             <input 
+                                class="accent-stone-950"
                                 bind:value={asset.portfolioWeight}
                                 type="range" 
                                 name={asset.symbol} 
@@ -207,12 +211,20 @@ function handleSliderChange(index: number, event: Event) {
                         </div>
                     {/each}
                 {/if}
-
-            <h1 class="text-4xl">Total Value: ${roundCash(total)}</h1>
+                <div class="flex text-4xl">
+                    {#if total >= 0}
+                    <h1 class="text-4x">Total Value: </h1>
+                    <h1 class="text-green-500">${roundCash(total)}</h1>
+                {:else}
+                
+                    <h1 class=" ">Total Value: </h1>
+                    <h1 class="text-red-700">${roundCash(total)}</h1>
+                {/if}
+                    </div>
         </div>
     </div>
-    <div class="row-span-4 col-span-2 flex items-center">
-        <canvas class="w-max h-1/2" id="myChart">
+    <div class="row-span-4 col-span-2 flex items-center bg-stone-950">
+        <canvas class="w-max h-1/2  text-white" id="myChart">
         </canvas>
     </div>
 </div>
